5434eaa5e537f78184ec94c8b330776311dcadc2
itai <itaimorag2000@gmail.com>
2022-12-20 15:28:04 +0200

    tube list

--- PlayList/src/components/tube-preview.vue
+++ PlayList/src/components/tube-preview.vue
@@ -1,14 +1,32 @@
 <template>
+    <section class="tube-preview" v-if="result">
+        <div>
+            <img :src="result.image.url">
+        </div>
+        <div class="text-song-area">
+            <h2>{{ result.title }}</h2>
+            <p>{{ result.description }}</p>
+        </div>
+        <div class="play-video-btn">
+            <button @click="selectSong">Play</button>
+        </div>
 
+    </section>
 
 </template>
 
 <script>
 export default {
     name: 'tube-preview',
-    props:{
-    result: Object,
-  },
+    emits: ['selectSong'],
+    props: {
+        result: Object,
+    },
+    methods: {
+        selectSong() {
+            this.$emit('selectSong', this.result)
+        },
+    },
 
 }
 
