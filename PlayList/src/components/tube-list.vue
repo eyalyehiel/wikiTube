9ea410129c4d2ccba89df5990e96dd550bec320d
itai <itaimorag2000@gmail.com>
2022-12-20 14:40:03 +0200

    tube list

--- PlayList/src/components/tube-list.vue
+++ PlayList/src/components/tube-list.vue
@@ -1,5 +1,43 @@
 <template>
     <section class="tube-list"></section>
+    <ul>
+        <li v-for="result in results">
+            <tubePreview :result="result"/>
+        </li>
+    </ul>
 </template>
 
-<script></script>
+<script>
+import tubePreview from "../components/tube-preview.vue"
+
+export default {
+  name: 'tube-list',
+  props:{
+    results: Array,
+  },
+  data() {
+    return {
+        chosenVideo:null,
+     
+    }
+  },
+  methods: {
+      setFilter(filterBy) {
+          this.$store.dispatch({ type: 'setFilter', filterBy })
+          // this.$store.dispatch({ type: 'loadToys' })
+        },
+        goToEdit() {
+            this.$router.push(`/toy/edit`)
+        },
+        removeToy(toyId) {
+            this.$store.dispatch({ type: 'removeToy', _id: toyId })
+        },
+    },
+    computed: {
+    },
+    components: {
+        tubePreview,
+    },
+}
+</script>
+
